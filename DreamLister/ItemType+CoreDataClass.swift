69b0b4f02a379a0d55394fbac7f4707febcc0d0a
William Henry Fulton IV <will.fulton95@gmail.com>
2017-02-26 23:22:30 -0500

    all entities are finished, organized data ib excite before ne

---
+++ DreamLister/ItemType+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by William H Fulton IV on 2/26/17.
+//  Copyright © 2017 Buddytime. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(ItemType)
+public class ItemType: NSManagedObject {
+
+}
---
+++ DreamLister/Store+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Store+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by William H Fulton IV on 2/26/17.
+//  Copyright © 2017 Buddytime. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Store)
+public class Store: NSManagedObject {
+
+}
