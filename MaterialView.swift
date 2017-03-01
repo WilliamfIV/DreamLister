7ac4b5c88b64610c4f8b1990416ae46cf27f7633
William Henry Fulton IV <will.fulton95@gmail.com>
2017-02-28 23:59:43 -0500

    work on table view and table view cell

---
+++ MaterialView.swift
@@ -0,0 +1,15 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by William H Fulton IV on 2/28/17.
+//  Copyright © 2017 Buddytime. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+//this is for adding styling to UIView
+
+}
