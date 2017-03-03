69d462c1cd52a2137b4e9eeee4edddec1c34b501
William Henry Fulton IV <will.fulton95@gmail.com>
2017-03-03 01:02:38 -0500

    added code to table cell and new classes

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by William H Fulton IV on 3/2/17.
+//  Copyright © 2017 Buddytime. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
